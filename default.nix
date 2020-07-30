0c7510c5a2019290d711187ac1efd2dfbc58fa73
Andrew Redinbo <atredinbo@gmail.com>
2020-07-29 21:55:49 -0400

    first commit

---
+++ default.nix
@@ -0,0 +1,69 @@
+# default.nix --- the heart of my dotfiles
+#
+# Author:  Andrew Redinbo <atredinbo@gmail.com>
+#          Henrik Lissner <henrik@lissner.net>
+# URL:     https://github.com/atred/dots
+# License: MIT
+#
+# This is ground zero, where the absolute essentials go, to be present on all
+# systems I use nixos on. Most of which are single user systems (the ones that
+# aren't are configured from their hosts/*/default.nix).
+
+device: username:
+{ pkgs, options, lib, config, ... }:
+{
+  networking.hostName = lib.mkDefault device;
+  my.username = username;
+
+  imports = [
+    ./modules
+    "${./hosts}/${device}"
+  ];
+
+  ### NixOS
+  nix.autoOptimiseStore = true;
+  nix.nixPath = options.nix.nixPath.default ++ [
+    # So we can use absolute import paths
+    "bin=/etc/dots/bin"
+    "config=/etc/dots/config"
+  ];
+
+  # Add custom packages & unstable channel, so they can be accessed via pkgs.*
+  nixpkgs.overlays = import ./packages;
+
+  # These are the things I want installed on all my systems
+  environment.systemPackages = with pkgs; [
+    # Just the bare essentials
+    coreutils
+    git
+    killall
+    unzip
+    neovim
+    wget
+    sshfs
+    ripgrep
+    gnumake # for makefile
+  ];
+
+  environment.shellAliases = {
+    nix-shell = ''NIX_PATH="nixpkgs-overlays=/etc/dotfiles/packages/default.nix:$NIX_PATH" nix-shell'';
+    nsh = "nix-shell";
+    nen = "nix-env";
+    dots = "make -C ~/dots"
+  };
+
+  # Default settings for primary user account. `my` is defined in
+  # modules/default.nix
+  my.user = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = [ "wheel" "networkmanager" ];
+    shell = pkgs.zsh;
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "20.03"; # Did you read the comment?
+}
